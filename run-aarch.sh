dadad85ff394ed21079a23536cd682df1120bea9
Josh Junon <git@josh.junon.me>
2024-03-23 02:42:00 +0100

    tweaks

--- run-aarch.sh
+++ run-aarch.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # QEMU_EFI.fd pulled from: https://releases.linaro.org/components/kernel/uefi-linaro/16.02/release/qemu64/
-exec qemu-system-aarch64 -M virt -cpu cortex-a57  -no-reboot -no-shutdown -serial stdio -cdrom ./oro-aarch64.iso -m 512 -smp cores=4 -bios ./QEMU_EFI.fd
+exec qemu-system-aarch64 -M virt -cpu cortex-a57  -no-reboot -no-shutdown -serial stdio -cdrom ./oro-aarch64.iso -m 512 -smp cores=2 -bios ./QEMU_EFI.fd "$@"
--- run-x86_64.sh
+++ run-x86_64.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-exec qemu-system-x86_64 -cdrom ./oro-x86_64.iso -serial stdio -no-reboot -no-shutdown -smp cores=4
+exec qemu-system-x86_64 -cdrom ./oro-x86_64.iso -serial stdio -no-reboot -no-shutdown -smp cores=4 "$@"
